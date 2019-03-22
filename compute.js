ea4ec1a82a43eb8fed9d415a2bfeb20b53a563bd
yanxingzhe <yanxingzhe@pinduoduo.com>
2019-03-23 00:10:00 +0800

    compute

--- compute.js
+++ compute.js
@@ -3,24 +3,41 @@ const fs = require("fs");
 const path = require("path");
 
 const input = path.resolve("./input.text");
-// const output = path.resolve("./output.json");
+const output = path.resolve("./output.md");
 
 const data = fs.readFileSync(input, "utf8");
 
-console.log("data", data, typeof data);
-// const { left, top, eles } = JSON.parse(data);
+// console.log("data", data, typeof data);
 
-// let maxId = 1;
-// eles.forEach(item => {
-//   if (item.id > maxId) {
-//     maxId = item.id;
-//   }
-// });
+const arr = data
+  .replace(/ \| /g, " ")
+  .slice(0, data.lastIndexOf("}"))
+  .split("/**");
+const interfaceName = arr[0].match(/interface (.*) {/)[1];
+const attrs = arr.slice(1);
+let result = "";
 
-// const result = eles.map(ele => ({
-//   ...ele,
-//   left: ele.left + left,
-//   top: ele.top + top,
-//   id: ++maxId
-// }));
-// fs.writeFileSync(output, JSON.stringify(result, null, 2));
+result = attrs
+  .map(attr => {
+    attr = attr.split("*/");
+    let desc = attr[0].replace(/[\* | \n]/g, " ").trim();
+    let keyVal = attr[1].match(/(.*)[?]:(.*)/);
+
+    return keyVal && keyVal.length > 2
+      ? `| ${keyVal[1]} | ${desc} | ${keyVal[2]} | - |
+  `
+      : "";
+  })
+  .join("");
+
+let newData;
+newData = `
+interfaceName: ${interfaceName}
+
+| 属性              |                                       说明                                       |              类型              |          默认值 |
+| ----------------- | :------------------------------------------------------------------------------: | :----------------------------: | --------------: |
+| trigger           |                                  触发弹层的事件                                  |   hover, focus, click    |         `.concat(
+  result
+);
+
+fs.writeFileSync(output, newData);
---
+++ output.md
@@ -0,0 +1,20 @@
+
+interfaceName: BaseButtonProps
+
+| 属性              |                                       说明                                       |              类型              |          默认值 |
+| ----------------- | :------------------------------------------------------------------------------: | :----------------------------: | --------------: |
+| trigger           |                                  触发弹层的事件                                  |   hover, focus, click    |         |   type | 'primary' 'default' 'ghost' 'gray' 'text-primary' 'text' 'text-gray'      @default 'default' |  ButtonType | - |
+  |   icon | 自定义按钮内部图标，我们有自己的图标库 |  string | - |
+  |   size | 设置按钮大小 'large' 'default' 'small'       @default 'default' |  ButtonSize | - |
+  |   loading | 是否显示loading |  boolean | - |
+  |   disabled | 是否可以点击 |  boolean | - |
+  |   htmlType | 'submit' 'button' 'reset'      设置 button 原生的 type 值，可选值请参考 HTML 标准，默认值为“button”      @default 'button' |  ButtonHTMLType | - |
+  |   prefixCls | className 前缀，一般情况下不要随意改动 |  string | - |
+  |   className | 自定义className,追加形式 |  string | - |
+  |   children | 按钮内容 |  React.ReactNode | - |
+  |   onClick | 点击事件 |  React.MouseEventHandler<HTMLButtonElement HTMLAnchorElement> | - |
+  |   href | type = "text " 类型的情况下使用 |  string | - |
+  |   target | type = "text " 类型的情况下使用 |  string | - |
+  |   style | 自定义样式 |  React.CSSProperties | - |
+  |   autodisable | autodisable 是否禁用 |  boolean | - |
+  
